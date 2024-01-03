b39b936209a124cb1d5911a2941eddb9371f0576
Matheus Moreira <mthsmoreira59@gmail.com>
2024-01-03 20:53:00 -0300

    implementa outra classe

---
+++ class/VipAccount.ts
@@ -0,0 +1,13 @@
+import { DioAccount } from './DioAccount'
+
+export class VipAccount extends DioAccount {
+  private _balance: number = this.getBalance()
+
+  constructor(name: string, accountNumber: number) {
+    super(name, accountNumber)
+  }
+
+  deposit = (value: number) => {
+    this._balance = this._balance + (value + 10)
+  }
+}
