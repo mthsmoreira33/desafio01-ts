75c7e1473aeb4f5128b430a190a7ffb9c700ec20
Matheus Moreira <mthsmoreira59@gmail.com>
2024-01-03 13:09:58 -0300

    testa metodos

--- app.ts
+++ app.ts
@@ -3,7 +3,8 @@ import { PeopleAccount } from './class/PeopleAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
 console.log(peopleAccount)
-peopleAccount.deposit()
+peopleAccount.deposit(100)
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
+companyAccount.deposit(200)
 console.log(companyAccount)
+console.log(peopleAccount)
